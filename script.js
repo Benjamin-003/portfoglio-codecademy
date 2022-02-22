6fa0fce67017789fe46f36ef7bc7af13bacb7265
Benjamin Boissin <benjamin.boissin@modis.com>
2022-02-22 15:17:27 +0100

    fix event icones

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const jsElement = document.getElementById('js');
 const phpElement = document.getElementById('php');
 const angularElement = document.getElementById('angular');
 
-htmlElement.addEventListener('mouseover', function (event) {
+htmlElement.addEventListener('click', function (event) {
    popup = window.open("https://fr.wikipedia.org/wiki/Hypertext_Markup_Language","popUpWindow","width=1200,height=1000,scrollbars=yes");
   })
 
-  cssElement.addEventListener('mouseover', function (event) {
+  cssElement.addEventListener('click', function (event) {
     popup = window.open("https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade","popUpWindow","width=1200,height=1000,scrollbars=yes");
    })
   
-   jsElement.addEventListener('mouseover', function (event) {
+   jsElement.addEventListener('click', function (event) {
     popup = window.open("https://fr.wikipedia.org/wiki/JavaScript","popUpWindow","width=1200,height=1000,scrollbars=yes");
    })
   
-   phpElement.addEventListener('mouseover', function (event) {
+   phpElement.addEventListener('click', function (event) {
     popup = window.open("https://fr.wikipedia.org/wiki/PHP","popUpWindow","width=1200,height=1000,scrollbars=yes");
    })
 
-   angularElement.addEventListener('mouseover', function (event) {
+   angularElement.addEventListener('click', function (event) {
     popup = window.open("https://fr.wikipedia.org/wiki/Angular","popUpWindow","width=1200,height=1000,scrollbars=yes");
    })
